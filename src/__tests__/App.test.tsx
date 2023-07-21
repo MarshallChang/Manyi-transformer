9a37e9599115b2853992e13085000568cb75ae69
marshall <marshallchang@qq.com>
2023-07-21 16:21:59 +0800

    fix: jest error

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -2,6 +2,21 @@ import '@testing-library/jest-dom';
 import { render } from '@testing-library/react';
 import App from '../renderer/App';
 
+const electron = {
+  ipcRenderer: {
+    sendMessage: jest.fn(),
+    invoke: jest.fn(),
+    on: jest.fn(),
+    once: jest.fn(),
+  },
+  store: {
+    get: jest.fn(),
+    set: jest.fn(),
+  },
+};
+
+window.electron = electron;
+
 describe('App', () => {
   it('should render', () => {
     expect(render(<App />)).toBeTruthy();
